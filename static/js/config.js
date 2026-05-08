9dae7e1b4bd739cd0dc6dd26ea78836257bcebbd
xtzeleven <33933215+xtzeleven@users.noreply.github.com>
2026-05-08 11:22:30 +0800

    Delete static directory

--- static/js/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
-    //url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
-    url: "https://api.openai.com/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: ""  // openAi Base64 编码 apiKey
-};  
